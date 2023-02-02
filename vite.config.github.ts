27a27fc49c4bbd8f833ac578f2e9e25286e03626
ZhangYahan <yahan233@163.com,zyahan1997@163.com>
2023-02-02 23:22:26 +0800

    update vite config github file

--- vite.config.github.ts
+++ vite.config.github.ts
@@ -5,14 +5,17 @@ import { defineConfig } from 'vite'
 const input: { [name: string]: string } = {
   main: resolve(__dirname, 'index.html'),
 }
-const example = fs.readdirSync(resolve(__dirname, 'examples'))
-for (const file of example) {
-  if (file.endsWith('.html')) {
-    input[file.slice(0, -5)] = resolve(__dirname, `example/${file}`)
+const examples = fs.readdirSync(resolve(__dirname, 'examples'))
+for (const example of examples) {
+  const files = fs.readdirSync(resolve(__dirname, 'examples', example))
+  for (const file of files) {
+    if (file.endsWith('.html')) {
+      input[file.slice(0, -5)] = resolve(__dirname, `examples/${example}/${file}`)
+    }
   }
 }
 module.exports = defineConfig({
-  base: '/three-example/',
+  base: '/three-examples/',
   build: {
     rollupOptions: {
       input,
